b206256554d6c4e1d6b911607c774524af1894ab
j-mv <jmv@uc.cl>
2025-05-25 19:40:51 -0400

    fix error search bar

--- vue-mybooks/src/components/search/SearchBar.vue
+++ vue-mybooks/src/components/search/SearchBar.vue
@@ -1,43 +1,3 @@
-<template>
-  <div class="search-wrapper">
-    <div class="search-container">
-      <div class="search-bar">
-        <svg
-          class="search-icon"
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <circle cx="11" cy="11" r="8"></circle>
-          <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-        </svg>
-
-        <input
-          class="search-input"
-          type="text"
-          :placeholder="searchType === 'book' ? 'Buscar libros...' : 'Buscar autores...'"
-          v-model="searchQuery"
-          @input="handleInput"
-          @keyup.enter="emitSearch"
-        />
-
-        <select v-model="searchType" @change="emitFilterChange">
-          <option value="book">Libro</option>
-          <option value="author">Autor</option>
-        </select>
-      </div>
-    </div>
-
-    <div v-if="isLoading" class="loading">Buscando...</div>
-  </div>
-</template>
-
 <script setup>
 import { ref, watch, onUnmounted } from 'vue'
 
@@ -105,6 +65,46 @@ function emitFilterChange() {
 
 onUnmounted(() => clearTimeout(searchTimeout))
 </script>
+
+<template>
+  <div class="search-wrapper">
+    <div class="search-container">
+      <div class="search-bar">
+        <svg
+          class="search-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <circle cx="11" cy="11" r="8"></circle>
+          <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+        </svg>
+
+        <input
+          class="search-input"
+          type="text"
+          :placeholder="searchType === 'book' ? 'Buscar libros...' : 'Buscar autores...'"
+          v-model="searchQuery"
+          @input="handleInput"
+          @keyup.enter="emitSearch"
+        />
+
+        <select v-model="searchType" @change="emitFilterChange">
+          <option value="book">Libro</option>
+          <option value="author">Autor</option>
+        </select>
+      </div>
+    </div>
+
+    <div v-if="isLoading" class="loading">Buscando...</div>
+  </div>
+</template>
 
 <style scoped>
 .search-wrapper {
